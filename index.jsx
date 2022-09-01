6c97f5644f3571bbd6c1375f6d1a8a7cc4524769
nickith21 <92686124+nickith21@users.noreply.github.com>
2022-09-01 18:26:09 -0400

    upload to GitHub

---
+++ index.jsx
@@ -0,0 +1,63 @@
+const ATMDeposit = ({ onChange, isDeposit }) => {
+  let choice = ["Withdrawal", "Deposit"];
+  let display = choice[Number(isDeposit)];
+  return (
+    <label className="label huge">
+      {display}
+      <input type="number" onChange={onChange}></input>
+      <input type="submit"></input>
+    </label>
+  );
+};
+
+const Account = () => {
+  const [accountBal, setAccountBal] = React.useState(0);
+  const [isDeposit, setIsDeposit] = React.useState(true);
+  const [deposit, setDeposit] = React.useState(0);
+
+  const handleChange = (event) => {
+    setDeposit(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let newTotal = isDeposit
+      ? accountBal + Number(deposit)
+      : accountBal - Number(deposit);
+    if (newTotal >= 0) {
+      setAccountBal(newTotal);
+      return;
+    }
+    alert("Cannot withdrawal more than account balance.");
+  };
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setIsDeposit(true);
+        }}
+      >
+        Deposit
+      </button>
+      <button
+        onClick={() => {
+          setIsDeposit(false);
+        }}
+      >
+        Withdrawal
+      </button>
+      <form onSubmit={handleSubmit}>
+        <h2>Account Balance ${accountBal}</h2>
+        <ATMDeposit onChange={handleChange} isDeposit={isDeposit}>
+          {" "}
+          Deposit
+        </ATMDeposit>
+      </form>
+    </>
+  );
+};
+// ========================================
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Account />);
